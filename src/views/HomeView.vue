6caf0141b05eaad51839a2f74a6125389730a2ef
Marlon <marlon_menezes@outlook.com>
2024-05-21 20:51:12 -0300

    Feat: Project ended

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import Card from '@/components/Card.vue';
 import quizzes from '@/data/quizzes.json';
 import { ref, watch } from 'vue';
+import gsap from 'gsap';
 
   const quizzesData = ref(quizzes)
   const search = ref('')
@@ -9,6 +10,21 @@ import { ref, watch } from 'vue';
   watch(search, (value) => {
     quizzesData.value = quizzes.filter(quiz => quiz.name.toLowerCase().includes(value.toLowerCase()))
   })
+
+  const beforeEnter = (el) => {
+    // card-enter-from
+    el.style.opacity = 0;
+    el.style.transform = 'translateY(-100px)';
+  }
+
+  const enter = (el) => {
+    gsap.to(el, {
+      opacity: 1,
+      y: 0, // transform: translateY(0)
+      duration: 0.3, // 400ms
+      delay: el.dataset.index * 0.3
+    })
+  }
 </script>
 
 <template>
@@ -18,11 +34,17 @@ import { ref, watch } from 'vue';
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <TransitionGroup name="card" appear>
+      <TransitionGroup 
+        name="card" 
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+      >
         <Card 
-          v-for="quiz in quizzesData" 
+          v-for="(quiz, index) in quizzesData" 
           :key="quiz.id" 
           :quizInfo="quiz"
+          :data-index="index"
         />
       </TransitionGroup>
     </div>
@@ -53,34 +75,5 @@ import { ref, watch } from 'vue';
     display: flex;
     flex-wrap: wrap;
     margin-top: 40px;
-  }
-
-  /* CARD */
-
-  .card-enter-from{
-    opacity: 0;
-    transform: translateY(-50px);
-  }
-  .card-enter-to{
-    opacity: 1;
-    transform: translateY(0);
-  }
-
-  .card-leave-from{
-    opacity: 1;
-    transform: translateY(0);   
-  }
-
-  .card-leave-to{
-    opacity: 0;
-    transform: translateY(-50px);
-  }
-
-  .card-enter-active, .card-leave-active{
-    transition: all 0.4s ease;
-  }
-
-  .card-move{
-    transition: transform 0.4s ease;
   }
 </style>
